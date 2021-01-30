403793fbed7134371194a525e9b54d1be43de02c
Orodan <jimmykasprzak@gmail.com>
2021-01-30 19:12:29 +0100

    💄 create mobile menu ui

--- app/js/app.js
+++ app/js/app.js
@@ -6,9 +6,11 @@ const overlay = document.querySelector('#overlay');
 
 mobileMenuBtn.addEventListener('click', () => {
     if (mobileMenu.classList.contains('open')) {
+        mobileMenuBtn.classList.remove('open')
         mobileMenu.classList.remove('open')
         overlay.classList.remove('open')
     } else {
+        mobileMenuBtn.classList.add('open')
         mobileMenu.classList.add('open')
         overlay.classList.add('open')
     }
