99e087f36319b580ec7632a4eee11df1fc833412
Orodan <jimmykasprzak@gmail.com>
2021-02-08 23:58:31 +0100

    ✨ allow slider to move left and right

--- app/js/app.js
+++ app/js/app.js
@@ -18,9 +18,14 @@ mobileMenuBtn.addEventListener('click', () => {
 
 // Slider
 
-const sliderImagesContainer = document.querySelector('.header__images')
+const sliderItemsContainer = document.querySelector('.header__images')
 const sliderControlLeft = document.querySelector('#slider-control-left')
 const sliderControlRight = document.querySelector('#slider-control-right')
+
+const sliderItemWidth = document.querySelector('#header').clientWidth
+console.log(sliderItemWidth)
+
+let currentWidthOffset = 0
 
 sliderControlLeft.addEventListener('click', () => {
     moveLeft()
@@ -32,8 +37,18 @@ sliderControlRight.addEventListener('click', () => {
 
 const moveLeft = () => {
     console.log('move left')
+
+    sliderItemsContainer.style.transition = 'transform .3s ease-in-out'
+    sliderItemsContainer.style.transform = `translateX(${currentWidthOffset + sliderItemWidth}px)`
+
+    currentWidthOffset += sliderItemWidth
 }
 
 const moveRight = () => {
     console.log('move right')
+
+    sliderItemsContainer.style.transition = 'transform .3s ease-in-out'
+    sliderItemsContainer.style.transform = `translateX(${currentWidthOffset - sliderItemWidth}px)`
+
+    currentWidthOffset -= sliderItemWidth
 }
